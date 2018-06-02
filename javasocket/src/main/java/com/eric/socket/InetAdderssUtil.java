8b29d9f41379b5d92220dce128c19f00e686aa1e
刘为红 <1981weihong@163.com>
2018-06-03 07:48:39 +0800

    liuweihong add

--- javasocket/src/main/java/com/eric/socket/InetAdderssUtil.java
+++ javasocket/src/main/java/com/eric/socket/InetAdderssUtil.java
@@ -4,9 +4,37 @@ import java.net.InetAddress;
 import java.net.UnknownHostException;
 
 public class InetAdderssUtil {
+    /**
+     * 从抓包看会从本地DNS开始查找
+     */
     public InetAddress getAddressByName(String name){
         try {
             InetAddress inetAddress =  InetAddress.getByName(name);
+            System.out.println(inetAddress);
+            return inetAddress;
+        } catch (UnknownHostException e) {
+            e.printStackTrace();
+        }
+        return null;
+
+    }
+    public InetAddress[] getAllAddressByName(String name){
+        try {
+            InetAddress[] inetAddress =  InetAddress.getAllByName(name);
+            for (int i = 0; i < inetAddress.length; i++) {
+                System.out.println("getAllAddressByName"+inetAddress[i]);
+
+            }
+            return inetAddress;
+        } catch (UnknownHostException e) {
+            e.printStackTrace();
+        }
+        return null;
+
+    }
+    public InetAddress getLocalHost(){
+        try {
+            InetAddress inetAddress =  InetAddress.getLocalHost();
             System.out.println(inetAddress);
             return inetAddress;
         } catch (UnknownHostException e) {
--- javasocket/src/test/java/com/eric/socket/InetAdderssUtilTest.java
+++ javasocket/src/test/java/com/eric/socket/InetAdderssUtilTest.java
@@ -8,7 +8,22 @@ public class InetAdderssUtilTest {
 
     @Test
     public void getAddressByName() {
+
         InetAdderssUtil inetAdderssUtil = new InetAdderssUtil();
-        inetAdderssUtil.getAddressByName("www.sina.com.cn");
+        inetAdderssUtil.getAddressByName("www.zte.com.cn");
+    }
+
+    @Test
+    public void getAllAddressByName(final String test) {
+        InetAdderssUtil inetAdderssUtil = new InetAdderssUtil();
+        inetAdderssUtil.getAllAddressByName("www.sina.com.com");
+
+    }
+
+    @Test
+    public void getLocalHost() {
+        InetAdderssUtil inetAdderssUtil = new InetAdderssUtil();
+        inetAdderssUtil.getLocalHost();
+
     }
 }
